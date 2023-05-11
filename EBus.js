b767f075571149aa2f6027dd3ece3c8df879a080
Febby315 <564822672@qq.com>
2023-05-11 12:02:03 +0800

    Update EBus.js
    优化

--- EBus.js
+++ EBus.js
@@ -1,43 +1,37 @@
 export class EBus {
   static __opKey = Symbol('ebus');
-  static normolizeHandler(handler, options) {
-    const op = Object.assign({ once: false, context: undefined }, EBus.options, options);
-    return Object.defineProperty(handler, EBus.__opKey, { value: op, writable: true, configurable: true });
+  static normalizeHandler(handler, options) {
+    if (!handler || handler.constructor !== Function) return;
+    if (!handler[EBus.__opKey]) Object.defineProperty(handler, EBus.__opKey, { value: {}, writable: true });
+    Object.assign(handler[EBus.__opKey], options);
+    return handler;
   }
-  static applyHandler = (handler, args) => {
-    const op = handler[EBus.__opKey];
-    handler.apply(op.context, args);
-    return op.once;
-  };
   constructor() {
     this._events = new Map();
   }
   on(type, handler) {
-    if (!handler || handler.constructor !== Function || !(handler instanceof Function)) return;
-    if (!Array.isArray(this._events.get(type))) this._events.set(type, []);
+    if (!EBus.normalizeHandler(handler)) return; // 无效处理器
     const handlers = this._events.get(type);
-    handlers.push(EBus.normolizeHandler(handler));
+    Array.isArray(handlers) ? handlers.push(handler) : this._events.set(type, [handler]);
+  }
+  once(type, handler) {
+    if (!EBus.normalizeHandler(handler, { once: true })) return; // 无效处理器
+    const handlers = this._events.get(type);
+    Array.isArray(handlers) ? handlers.push(handler) : this._events.set(type, [handler]);
   }
   off(type, handler) {
     const handlers = this._events.get(type) || [];
     const idx = handlers.indexOf(handler);
     if (idx !== -1) handlers.splice(idx, 1);
   }
-  once(type, handler) {
-    this.on(type, handler);
-    const op = handler[EBus.__opKey];
-    if (op) op.once = true; // 标记once
-  }
   emit(type, ...args) {
-    const handlers = this._events.get(type) || [];
-    const handlers_all = this._events.get('*') || [];
-    const onceList = handlers.filter(handler => EBus.applyHandler(handler, args));
-    const onceList_all = handlers_all.filter(handler => EBus.applyHandler(handler, args));
-    // 移除once监听器
-    onceList.map(handler => this.off(type, handler));
-    onceList_all.map(handler => this.on(type, handler));
+    const handlers = Object.assign([], this._events.get(type));
+    while (handlers.length) {
+      const handler = handlers.shift();
+      const op = handler[EBus.__opKey];
+      handler.apply(op.context, args);
+      if (op.once) this.off(type, handler); // 关闭一次性事件
+    }
+    if (type !== '*') this.emit('*', ...args); // 触发*监听器
   }
 }
-
-// eslint-disable-next-line no-undef
-globalThis.EBus = EBus;
